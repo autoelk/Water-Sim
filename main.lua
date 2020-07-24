4aa0e2a982592920400825c08d4fb276dbffea75
lukefly2 <lukefly2@outlook.com>
2020-07-24 19:59:53 -0400

    working water sim without class

--- main.lua
+++ main.lua
@@ -1,50 +1,62 @@
-require "Water"
-
 grid = {}
-numTiles = 10
-gridSize = math.floor(math.min(love.graphics.getWidth(), love.graphics.getHeight()) / tiles)
-
 function love.load()
-  t = 1 -- stores the current turn
-  -- add a second layer to the grid array that stores what the grid will look like on the next turn
-  for i = 1, numTiles do
+  for i=1,20 do
     grid[i] = {}
-    for j = 1, numTiles do
-      grid[i][j] = {}
-      for k = 1, 2 do
-        grid[i][j][k] = Water:Create("gas", 0, i, j)
+    for j=1,20 do
+      grid[i][j] = 0
+    end
+  end
+  grid[1][5] = 1
+  grid[5][5] = -1
+  grid[6][6] = -1
+  grid[6][7] = -1
+  turn = 1
+end
+
+function love.keypressed()
+  for i = 10, 2, -1 do
+    for j = 10, 2, -1 do
+      if grid[i - 1][j] > 0 and grid[i][j] ~= -1 then
+        grid[i][j] = grid[i][j] + grid[i - 1][j]
+        grid[i - 1][j] = 0
+        if grid[i + 1][j] == -1 then
+          if grid[i][j + 1] ~= grid[i][j] and grid[i][j - 1] ~= grid[i][j] and grid[i][j + 1] ~= -1  and grid[i][j - 1] ~= -1 then
+            newVol = (grid[i][j] + grid[i][j + 1] + grid[i][j - 1]) / 3
+            grid[i][j] = newVol
+            grid[i][j + 1] = newVol
+            grid[i][j - 1] = newVol
+          else
+            if grid[i][j + 1] ~= grid[i][j] and grid[i][j + 1] ~= -1 then
+              newVol = (grid[i][j] + grid[i][j + 1]) / 2
+              grid[i][j] = newVol
+              grid[i][j + 1] = newVol
+            end
+            if grid[i][j - 1] ~= grid[i][j] and grid[i][j - 1] ~= -1 then
+              newVol = (grid[i][j] + grid[i][j - 1]) / 2
+              grid[i][j] = newVol
+              grid[i][j - 1] = newVol
+            end
+          end
+        end
       end
     end
   end
-  grid[1][4] = Water:Create("liquid", 1, 1, 4)
-  grid[1][6] = Water:Create("liquid", 1, 1, 6)
-  grid[3][4] = Water:Create("solid", 1, 3, 4)
-  grid[4][5] = Water:Create("solid", 1, 4, 5)
-  grid[4][6] = Water:Create("solid", 1, 4, 6)
-  grid[5][7] = Water:Create("solid", 1, 5, 7)
-end
-
-function love.keypressed(key, isrepeat)
-  for i = 1, numTiles do
-    for j = 1, numTiles do
-      grid[i][j][cur]:Update()
-    end
-  end
-  t = t + 1
-  cur = t % 2 + 1
-  next = (cur + 1) % 2
 end
 
 function love.draw()
-  love.graphics.setBackgroundColor(1,1,1)
-  for i = 1, numTiles do
-    for j = 1, numTiles do
-      grid[i][j][t%2+1]:Draw()
+  love.graphics.setBackgroundColor(1, 1, 1)
+  for i=1,10 do
+    for j=1,10 do
+      if grid[i][j] == -1 then
+        love.graphics.setColor(0, 0, 1)
+        love.graphics.rectangle("fill", j * 50, i * 50, 50, 50)
+      end
+      if grid[i][j] > 0 then
+        love.graphics.setColor(0, 0, 1, 0.5)
+        love.graphics.rectangle("fill", j * 50, (i + 1 - grid[i][j]) * 50, 50, 50 * grid[i][j])
+      end
+      love.graphics.setColor(0, 0, 0)
+      love.graphics.print(math.floor(grid[i][j]*100)/100, j * 50, i * 50)
     end
-  end
-  for i = 1, numTiles do
-    love.graphics.setColor(0,0,0,0.4)
-    love.graphics.line(gridSize * i, 0, gridSize * i, gridSize * numTiles)
-    love.graphics.line(0, gridSize * i, gridSize * numTiles, gridSize * i)
   end
 end
