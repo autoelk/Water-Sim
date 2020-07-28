742f06baf7983db82fda0c6c1d9479ace6ce2924
lukefly2 <lukefly2@outlook.com>
2020-07-27 21:52:01 -0400

    working!

--- main.lua
+++ main.lua
@@ -1,62 +1,92 @@
 grid = {}
+gridSize = 10
+tileSize = 50
+
 function love.load()
-  for i=1,20 do
+  math.randomseed(os.time())
+  for i=1,gridSize + 1 do
     grid[i] = {}
-    for j=1,20 do
-      grid[i][j] = 0
+    for j=1,gridSize + 1 do
+      grid[i][j] = 0;
     end
   end
-  grid[1][5] = 1
-  grid[5][5] = -1
-  grid[6][6] = -1
-  grid[6][7] = -1
-  turn = 1
-end
 
-function love.keypressed()
-  for i = 10, 2, -1 do
-    for j = 10, 2, -1 do
-      if grid[i - 1][j] > 0 and grid[i][j] ~= -1 then
-        grid[i][j] = grid[i][j] + grid[i - 1][j]
-        grid[i - 1][j] = 0
-        if grid[i + 1][j] == -1 then
-          if grid[i][j + 1] ~= grid[i][j] and grid[i][j - 1] ~= grid[i][j] and grid[i][j + 1] ~= -1  and grid[i][j - 1] ~= -1 then
-            newVol = (grid[i][j] + grid[i][j + 1] + grid[i][j - 1]) / 3
-            grid[i][j] = newVol
-            grid[i][j + 1] = newVol
-            grid[i][j - 1] = newVol
-          else
-            if grid[i][j + 1] ~= grid[i][j] and grid[i][j + 1] ~= -1 then
-              newVol = (grid[i][j] + grid[i][j + 1]) / 2
-              grid[i][j] = newVol
-              grid[i][j + 1] = newVol
-            end
-            if grid[i][j - 1] ~= grid[i][j] and grid[i][j - 1] ~= -1 then
-              newVol = (grid[i][j] + grid[i][j - 1]) / 2
-              grid[i][j] = newVol
-              grid[i][j - 1] = newVol
-            end
-          end
-        end
+  for i=1,gridSize do
+    for j=1,gridSize do
+      x = math.random()
+      if x < 0.2 then
+        grid[i][j] = -1
+      elseif x < 0.4 then
+        grid[i][j] = 1
       end
     end
   end
+end
+
+timer = 0
+function love.update(dt)
+  timer = timer + dt
+  if timer >= 0.4 then
+    updateAll()
+    timer = 0
+  end
+end
+
+function love.keypressed()
+  updateAll()
 end
 
 function love.draw()
   love.graphics.setBackgroundColor(1, 1, 1)
-  for i=1,10 do
-    for j=1,10 do
+  for i=1,gridSize do
+    for j=1,gridSize do
       if grid[i][j] == -1 then
         love.graphics.setColor(0, 0, 1)
-        love.graphics.rectangle("fill", j * 50, i * 50, 50, 50)
+        love.graphics.rectangle("fill", j * tileSize, i * tileSize, tileSize, tileSize)
       end
       if grid[i][j] > 0 then
         love.graphics.setColor(0, 0, 1, 0.5)
-        love.graphics.rectangle("fill", j * 50, (i + 1 - grid[i][j]) * 50, 50, 50 * grid[i][j])
+        love.graphics.rectangle("fill", j * tileSize, (i + 1 - grid[i][j]) * tileSize, tileSize, tileSize * grid[i][j])
       end
-      love.graphics.setColor(0, 0, 0)
-      love.graphics.print(math.floor(grid[i][j]*100)/100, j * 50, i * 50)
+      -- love.graphics.setColor(0, 0, 0)
+      -- love.graphics.printf(math.floor(grid[i][j]*100)/100, j * 50, i * 50 + tileSize / 2, 50, "center")
     end
+  end
+end
+
+function updateAll()
+  for i = gridSize, 1, -1 do
+    for j = gridSize, 1, -1 do
+      if grid[i][j] > 0 and grid[i + 1][j] ~= -1 and grid[i + 1][j] < 1 then
+        newVol = grid[i + 1][j] + grid[i][j]
+        if (newVol > 1) then
+          grid[i + 1][j] = 1
+          grid[i][j] = newVol - 1
+        else
+          grid[i + 1][j] = newVol
+          grid[i][j] = 0
+        end
+      end
+      if grid[i][j] > 0 and (grid[i + 1][j] == -1 or grid[i + 1][j] >= 1) then
+        spread(i, j)
+      end
+    end
+  end
+end
+
+function spread(i, j)
+  if j + 1 <= gridSize and j - 1 >= 1 and grid[i][j + 1] ~= grid[i][j] and grid[i][j - 1] ~= grid[i][j] and grid[i][j + 1] ~= -1  and grid[i][j - 1] ~= -1 then
+    newVol = (grid[i][j] + grid[i][j + 1] + grid[i][j - 1]) / 3
+    grid[i][j] = newVol
+    grid[i][j + 1] = newVol
+    grid[i][j - 1] = newVol
+  elseif j + 1 <= gridSize and grid[i][j + 1] ~= grid[i][j] and grid[i][j + 1] ~= -1 then
+    newVol = (grid[i][j] + grid[i][j + 1]) / 2
+    grid[i][j] = newVol
+    grid[i][j + 1] = newVol
+  elseif j - 1 >= 1 and grid[i][j - 1] ~= grid[i][j] and grid[i][j - 1] ~= -1 then
+    newVol = (grid[i][j] + grid[i][j - 1]) / 2
+    grid[i][j] = newVol
+    grid[i][j - 1] = newVol
   end
 end
